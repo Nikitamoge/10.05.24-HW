5cd17f134f6cef2bc3d408e4c5d5a0bb9e074178
denyf <denyf@DENYS_PRYTULA>
2024-10-18 22:44:24 +0200

    Добавьте файлы проекта.

---
+++ 10.05.24 HW/Program.cs
@@ -0,0 +1,95 @@
+﻿//Task 1
+public class Play : IDisposable
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public int Year { get; set; }
+    private bool disposed = false;
+
+    public Play(string title, string author, string genre, int year)
+    {
+        Title = title;
+        Author = author;
+        Genre = genre;
+        Year = year;
+    }
+
+    ~Play()
+    {
+        Dispose(false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+               
+            }
+            disposed = true;
+        }
+    }
+}
+
+//Task 2
+public class Store : IDisposable
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Type { get; set; }
+    private bool disposed = false;
+
+    public Store(string name, string address, string type)
+    {
+        Name = name;
+        Address = address;
+        Type = type;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+            }
+            disposed = true;
+        }
+    }
+
+    ~Store()
+    {
+        Dispose(false);
+    }
+}
+
+//Task 3
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var play = new Play("Hamlet", "William Shakespeare", "Tragedy", 1600))
+        {
+            Console.WriteLine($"{play.Title}, {play.Author}, {play.Genre}, {play.Year}");
+        }
+
+        using (var store = new Store("SuperStore", "123 Main St", "Grocery"))
+        {
+            Console.WriteLine($"{store.Name}, {store.Address}, {store.Type}");
+        }
+    }
+}
